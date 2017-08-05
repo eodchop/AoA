1543689f1ddc72fdde8113b68244a23afb0b53b5
Caleb Gasser <calebgasser@gmail.com>
2017-08-05 13:51:04 -0500

    Added AjaxCalls class.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,24 @@
-console.log("test");
+class AjaxCalls {
+    static dndMonstersAPI(name, callback) {
+        var baseURL = "http://www.dnd5eapi.co/api/";
+        var optionsURL = "monsters/?name=" + name;
+
+        $.ajax({
+            url: baseURL + optionsURL,
+            type: 'GET',
+            dataType: 'json'
+        }).done(function(data) {
+            AjaxCalls.dndByURLAPI(data.results[0].url, callback);
+        })
+    }
+
+    static dndByURLAPI(urlToUse, callback) {
+        $.ajax({
+            url: urlToUse,
+            type: 'GET',
+            dataType: 'json'
+        }).done(function(data) {
+            callback(data);
+        })
+    }
+}
