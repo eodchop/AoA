ad76806084dca9669068212616256a106fbf161b
Josh Lehne <jdlehne@gmail.com>
2017-08-07 08:44:39 -0500

    added function to generate command list in modal

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,156 +1,139 @@
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyBhpg16Jjfu0asRXBKRhIzAPQFA7SsvYJQ",
-  authDomain: "another-online-adventure.firebaseapp.com",
-  databaseURL: "https://another-online-adventure.firebaseio.com",
-  projectId: "another-online-adventure",
-  storageBucket: "another-online-adventure.appspot.com",
-  messagingSenderId: "550710539266"
+    apiKey: "AIzaSyBhpg16Jjfu0asRXBKRhIzAPQFA7SsvYJQ",
+    authDomain: "another-online-adventure.firebaseapp.com",
+    databaseURL: "https://another-online-adventure.firebaseio.com",
+    projectId: "another-online-adventure",
+    storageBucket: "another-online-adventure.appspot.com",
+    messagingSenderId: "550710539266"
 };
 firebase.initializeApp(config);
 var database = firebase.database();;
 //A simple wrapper class to make ajax calls to the api's we use a little
 //bit easier.
 class AjaxCalls {
-  //Used to return the stats of a monster from the dnd api by passing the name.
-  static dndMonstersAPI(name, callback) {
-    var baseURL = "http://www.dnd5eapi.co/api/";
-    var optionsURL = "monsters/?name=" + name;
+    //Used to return the stats of a monster from the dnd api by passing the name.
+    static dndMonstersAPI(name, callback) {
+        var baseURL = "http://www.dnd5eapi.co/api/";
+        var optionsURL = "monsters/?name=" + name;
 
-    $.ajax({
-      url: baseURL + optionsURL,
-      type: 'GET',
-      dataType: 'json'
-    }).done(function(data) {
-      AjaxCalls.dndByURLAPI(data.results[0].url, callback);
-    })
-  }
+        $.ajax({
+            url: baseURL + optionsURL,
+            type: 'GET',
+            dataType: 'json'
+        }).done(function(data) {
+            AjaxCalls.dndByURLAPI(data.results[0].url, callback);
+        })
+    }
 
-  static dndByURLAPI(urlToUse, callback) {
-    $.ajax({
-      url: urlToUse,
-      type: 'GET',
-      dataType: 'json'
-    }).done(function(data) {
-      callback(data);
-    })
-  }
+    static dndByURLAPI(urlToUse, callback) {
+        $.ajax({
+            url: urlToUse,
+            type: 'GET',
+            dataType: 'json'
+        }).done(function(data) {
+            callback(data);
+        })
+    }
 }
 
 //Utility functions that come in handy everywhere.
 var Utils = {
-  toTitleCase: function(str) {
-    return str.replace(/\w\S*/g, function(txt) {
-      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    });
-  },
-  locationDataReformat: function(locationString) {
-    locationString = locationString.replace("_", " ");
-    locationString = Utils.toTitleCase(locationString);
-    return locationString;
-  },
-  reformatToLocationData: function(locationString) {
-    locationString = locationString.toLowerCase();
-    locationString = locationString.replace(" ", "_");
-    return locationString;
-  },
-  isEmptyObj: function(obj) {
-    for(var key in obj) {
-        if(obj.hasOwnProperty(key))
-            return false;
+    toTitleCase: function(str) {
+        return str.replace(/\w\S*/g, function(txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        });
+    },
+    locationDataReformat: function(locationString) {
+        locationString = locationString.replace("_", " ");
+        locationString = Utils.toTitleCase(locationString);
+        return locationString;
+    },
+    reformatToLocationData: function(locationString) {
+        locationString = locationString.toLowerCase();
+        locationString = locationString.replace(" ", "_");
+        return locationString;
+    },
+    isEmptyObj: function(obj) {
+        for (var key in obj) {
+            if (obj.hasOwnProperty(key))
+                return false;
+        }
+        return true;
     }
-    return true;
-}
 }
 //---sorting out creating and storing characters
 
 var userCharacter = {
 
-  name: "",
-  location: "",
-  description: "",
-  characterClass: "",
-  //example placeholder stats
-  str: 0,
-  dex: 0,
-  health: 0,
-<<<<<<< HEAD
-  constitution: 0,
-=======
-  constitution:0,
->>>>>>> 549322aa502aa3dbeb040defc647d386fa0a63d9
+    name: "",
+    location: "",
+    description: "",
+    characterClass: "",
+    //example placeholder stats
+    str: 0,
+    dex: 0,
+    health: 0,
+    constitution: 0,
+
 };
 
 
 $(document).ready(function() {
 
 
-  //---Recording Character name and Description and writing to game page--//
-  $("#charLoadBtn").click(function() {
+    //---Recording Character name and Description and writing to game page--//
+    $("#charLoadBtn").click(function() {
 
-    name = $("#playerName").val().trim();
-    $("#nameDisplay").html(name);
-    userCharacter.name = name;
-    console.log(name);
-<<<<<<< HEAD
+        name = $("#playerName").val().trim();
+        $("#nameDisplay").html(name);
+        userCharacter.name = name;
+        console.log(name);
 
-    description = $("#playerDescription").val().trim();
-    $("#descript").html(description);
-    userCharacter.description = description;
-    console.log(description);
 
-  });
+        description = $("#playerDescription").val().trim();
+        $("#descript").html(description);
+        userCharacter.description = description;
+        console.log(description);
 
-  $("#classSelector li a").click(function() {
-    characterClass = $(this).text();
-    console.log(characterClass);
-    userCharacter.characterClass = characterClass;
-    $("#playerClass").html(characterClass);
-  });
+    });
 
-//  $("#commandBtn").click(function() {
-  //  $('#commandBox').animate({
-    //  left: endPos
-  //  }, 1000);
-//  });
+    $("#classSelector li a").click(function() {
+        characterClass = $(this).text();
+        console.log(characterClass);
+        userCharacter.characterClass = characterClass;
+        $("#playerClass").html(characterClass);
+    });
+//------------on click to generate command list on command Modal-----------//
+    $("#commandBtn").click(function() {
+        for (var i in commands) {
+            if (commands.hasOwnProperty(i)) {
+                console.log(i + " -> " + commands[i].syntax + commands[i].description);
+                var commandEntry = $("<p class='commandText'>").html(commands[i].syntax + ": " + "<br>" + commands[i].description);
+                $("#commandBox").append(commandEntry);
+                event.preventDefault();
+            }
+        }
+
+    });
 
 });
-
-
 
 
 //---building command object to append to user instruction modal---//
 
 var commands = {
-  say: {
-    syntax: "/say or /s",
-    description: "Used to say something in gobal chat"
-  },
-  map: {
-    syntax: "/map or /m",
-    description: "Used to display surrounding locations to travel"
-  },
+    say: {
+        syntax: "/say or /s",
+        description: "Used to say something in gobal chat"
+    },
+    map: {
+        syntax: "/map or /m",
+        description: "Used to display surrounding locations to travel"
+    },
 
-  inspect: {
-    syntax: "/inspect"
-    description: "Used to gather information on things in environment"
-  }
-}
-=======
-
-    description = $("#playerDescription").val().trim();
-    $("#descript").html(description);
-    userCharacter.description = description;
-    console.log(description);
-
-  });
-
-  $("#classSelector li a").click(function() {
-    characterClass = $(this).text();
-    console.log(characterClass);
-    userCharacter.characterClass = characterClass;
-    $("#playerClass").html(characterClass);
-  });
-
-});
->>>>>>> 549322aa502aa3dbeb040defc647d386fa0a63d9
+    inspect: {
+        syntax: "/inspect",
+        description: "Used to gather information on things in environment"
+    }
+};
