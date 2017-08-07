90e43e39888f1efc924b4c25a847a39be166024f
Ryan Southard <eodchop@gmail.com>
2017-08-07 18:30:35 -0500

    added login.js script tag.

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -6,7 +6,7 @@ firebase.auth().signInWithPopup(provider).then(function(result) {
     var token = result.credential.accessToken;
     // The signed-in user info.
     var user = result.user;
-    // ...
+    console.log(user);
 }).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
