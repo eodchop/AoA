c91eac9c51c1ac2b950b948806747bee0d763277
Caleb Gasser <calebgasser@gmail.com>
2017-08-12 11:58:01 -0500

    Background music volume change.

--- assets/scripts/sounds.js
+++ assets/scripts/sounds.js
@@ -1,30 +1,21 @@
-  var SoundManager = {
-      messagePop: new Audio('./assets/sounds/messagePop.wav'),
-      littleTown: new Audio('./assets/sounds/littleTown.ogg'),
-      deathTone: new Audio('./assets/sounds/orc_die.ogg'),
-      playMessagePopOnce: function () {
-      if (SoundManager.messagePop.paused) {
-          SoundManager.messagePop.currentTime = 0;
-          SoundManager.messagePop.play();
-      } else {
-          SoundManager.messagePop.pause();
-      }
+var SoundManager = {
+  messagePop: new Audio('./assets/sounds/messagePop.wav'),
+  littleTown: new Audio('./assets/sounds/littleTown.ogg'),
+  playMessagePopOnce: function(){
+    if(SoundManager.messagePop.paused){
+      SoundManager.messagePop.currentTime = 0;
+      SoundManager.messagePop.play();
+    } else {
+      SoundManager.messagePop.pause();
+    }
   },
-  playBackgroundMusicLoop: function () {
-      if (SoundManager.littleTown.paused) {
-          SoundManager.littleTown.loop = true;
-          SoundManager.littleTown.play();
-      } else {
-          SoundManager.littleTown.pause();
-      }
-  },
-      playDeathSound: function () {
-      if (SoundManager.deathTone.paused) {
-          SoundManager.deathTone.currentTime = 0;
-          SoundManager.deathTone.play();
-      } else {
-          SoundManager.deathTone.pause();
-      }
-  },
+  playBackgroundMusicLoop: function(){
+    if(SoundManager.littleTown.paused){
+      SoundManager.littleTown.volume = 0.3;
+      SoundManager.littleTown.loop = true;
+      SoundManager.littleTown.play();
+    } else {
+      SoundManager.littleTown.pause();
+    }
   }
-
+}
