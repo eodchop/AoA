a37fccb18b56be2bbacf292a7571d5623d62d2bb
Ryan Southard <eodchop@gmail.com>
2017-08-12 11:33:35 -0500

    added death sound

--- assets/scripts/sounds.js
+++ assets/scripts/sounds.js
@@ -1,20 +1,30 @@
-var SoundManager = {
-  messagePop: new Audio('./assets/sounds/messagePop.wav'),
-  littleTown: new Audio('./assets/sounds/littleTown.ogg'),
-  playMessagePopOnce: function(){
-    if(SoundManager.messagePop.paused){
-      SoundManager.messagePop.currentTime = 0;
-      SoundManager.messagePop.play();
-    } else {
-      SoundManager.messagePop.pause();
-    }
+  var SoundManager = {
+      messagePop: new Audio('./assets/sounds/messagePop.wav'),
+      littleTown: new Audio('./assets/sounds/littleTown.ogg'),
+      deathTone: new Audio('./assets/sounds/orc_die.ogg'),
+      playMessagePopOnce: function () {
+      if (SoundManager.messagePop.paused) {
+          SoundManager.messagePop.currentTime = 0;
+          SoundManager.messagePop.play();
+      } else {
+          SoundManager.messagePop.pause();
+      }
   },
-  playBackgroundMusicLoop: function(){
-    if(SoundManager.littleTown.paused){
-      SoundManager.littleTown.loop = true;
-      SoundManager.littleTown.play();
-    } else {
-      SoundManager.littleTown.pause();
-    }
+  playBackgroundMusicLoop: function () {
+      if (SoundManager.littleTown.paused) {
+          SoundManager.littleTown.loop = true;
+          SoundManager.littleTown.play();
+      } else {
+          SoundManager.littleTown.pause();
+      }
+  },
+      playDeathSound: function () {
+      if (SoundManager.deathTone.paused) {
+          SoundManager.deathTone.currentTime = 0;
+          SoundManager.deathTone.play();
+      } else {
+          SoundManager.deathTone.pause();
+      }
+  },
   }
-}
+
